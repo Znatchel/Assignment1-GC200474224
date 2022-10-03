ded1e0268014fcf22d47de35880cfb9e362a7f55
Znatchel <Mitchellryan.work@gmail.com>
2022-10-02 20:50:32 -0400

    Lab Week 4 Initial Commit

---
+++ src/main/java/com/example/assignment1gc200474224/Controller.java
@@ -0,0 +1,14 @@
+package com.example.assignment1gc200474224;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
